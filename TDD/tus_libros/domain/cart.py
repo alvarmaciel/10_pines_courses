13e8ee4db5d24561f38d76321f0eb37e110a8456
Alvar Maciel <alvarmaciel@gmail.com>
2023-04-21 18:01:19 -0300

    third test fails

--- TDD/tus_libros/domain/cart.py
+++ TDD/tus_libros/domain/cart.py
@@ -1,9 +1,13 @@
 from abc import ABCMeta
+from datetime import datetime
+
+
 class Cart(metaclass=ABCMeta):
     def __init__(self):
         self._books = []
+        self._created_at = datetime.utcnow()
     def is_empty(self):
         return len(self._books)==0
 
-    def add(self, isbn):
-         self._books.append(isbn)
+    def add(self, isbn: str):
+        self._books.append(isbn)
--- TDD/tus_libros/tests/tests_tus_libros_domain.py
+++ TDD/tus_libros/tests/tests_tus_libros_domain.py
@@ -1,17 +1,43 @@
 import unittest
+
+from datetime import datetime, timedelta
+from freezegun import freeze_time
+
 from domain.cart import Cart
 
 
 
-class MyTestCase(unittest.TestCase):
+class CartTestCase(unittest.TestCase):
     def test_un_carro_nuevo_es_vacio(self):
         self.assertTrue(Cart().is_empty())  # add assertion here
 
     def test_agrega_un_libro_y_no_es_vacio(self):
-        isbn = 1
+        isbn = "1"
         cart = Cart()
         cart.add(isbn)
         self.assertFalse(cart.is_empty())
+
+    def test_agregas_mas_de_un_libro(self):
+        a_isbn = "1"
+        another_isbn = "1a"
+        cart = Cart()
+        cart.add(a_isbn)
+        cart.add(another_isbn)
+        self.assertFalse(cart.is_empty())
+        self.assertEqual(len(cart._books), 2)
+
+    def test_a_cart_with_books_gets_empty_after_x_lapse_of_time(self):
+        a_isbn = "1"
+        another_isbn = "1a"
+        cart = Cart()
+        cart.add(a_isbn)
+        cart.add(another_isbn)
+        delta = timedelta(minutes=30)
+        created_at = cart._created_at
+        with freeze_time((created_at + delta)):
+            self.assertTrue(cart.is_empty())
+
+
 
 if __name__ == '__main__':
     unittest.main()
