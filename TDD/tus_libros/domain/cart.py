fe12083ab7e5bb4232ffe843c47a926504cf73ae
Alvar Maciel <alvarmaciel@gmail.com>
2023-04-24 10:04:16 -0300

    volvemos a una estado basico

--- TDD/tus_libros/domain/cart.py
+++ TDD/tus_libros/domain/cart.py
@@ -5,12 +5,10 @@ from datetime import datetime, timedelta
 class Cart(metaclass=ABCMeta):
     def __init__(self):
         self._books = []
-        self._created_at = datetime.utcnow()
+
 
     def is_empty(self):
-        delta = timedelta(minutes=30)
-        if self._created_at + delta <= datetime.utcnow():
-            self._books.clear()
+
         return len(self._books) == 0
 
     def add(self, isbn: str):
